2833c1f594b810410093515cf16f640d1999c1de
Дмитрий <dimanng08@gmail.com>
2021-05-13 23:09:16 +0700

    Добавьте файлы проекта.

---
+++ Vector/Vector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector
+{
+    public class Vector
+    {
+        private double[] vector;
+
+        public Vector(int n)
+        {
+            try
+            {
+                vector = new double[n];
+                if (n <= 0)
+                {
+                    throw new ArgumentException("Указана недопустимая размерность вектора");
+                }
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Ошибка: " + e.Message);
+            }
+        }
+        public Vector(Vector copyVector)
+        {
+            vector = copyVector.vector;
+        }
+    }
+}
